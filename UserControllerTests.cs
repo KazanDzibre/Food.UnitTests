c023d3d5e5611bcf4cc261fdd6ee54517a3989b3
KazanDzibre <markoruzic997@gmail.com>
2021-06-17 20:34:42 +0200

    Radi

--- UserControllerTests.cs
+++ UserControllerTests.cs
@@ -1,7 +1,11 @@
 using System;
 using AutoMapper;
+using FluentAssertions;
 using Food.Configuration;
 using Food.Controllers;
+using Food.Core;
+using Food.Model;
+using Food.Service;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -10,20 +14,53 @@ namespace Food.UnitTests
 {
     public class UserControllerTests
     {
+
+        private readonly Mock<IMapper> mapperStub = new(); 
+        private readonly Mock<IUserService> _userService = new Mock<IUserService>();
+        private readonly Random rand = new();
+
         [Fact]
         public void GetUserById_WithUnexistingUser_ReturnsNotFound()
         {
             //Arrange
-            var mapperStub = new Mock<IMapper>();
-            ProjectConfiguration configuration = new ProjectConfiguration();
-            var Controller = new UserController(mapperStub.Object, configuration);
+            var Controller = new UserController(mapperStub.Object, _userService.Object);
             //Act
 
             var result = Controller.GetUserById(2);
             //Assert
 
             Assert.IsType<NotFoundResult>(result.Result);
+        }
+        [Fact]
+        public void GetUserById_WithExistingUser_ReturnsExpectedItem()
+        {
+            //Arrange
+            var ExpectedUser = CreateRandomUser();
+            _userService.Setup(p => p.GetUserById(2)).Returns(ExpectedUser);
+            var Controller = new UserController(mapperStub.Object, _userService.Object);
 
+            //Act
+            var result = (OkObjectResult)Controller.GetUserById(2).Result;
+            //Assert
+
+            result.Value.Should().BeEquivalentTo(ExpectedUser,
+                    options => options.ComparingByMembers<User>());
+        }
+
+        private User CreateRandomUser()
+        {
+            return new()
+            {
+                Id = 2,
+                FirstName = Guid.NewGuid().ToString(),
+                LastName = Guid.NewGuid().ToString(),
+                UserName = Guid.NewGuid().ToString(),
+                Type = Guid.NewGuid().ToString(),
+                latitude = rand.Next(45),
+                longitude = rand.Next(45),
+                RegistrationToken = "",
+                Password = Guid.NewGuid().ToString()
+            };
         }
     }
 }
